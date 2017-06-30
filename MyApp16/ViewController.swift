10a7fa7e58312cb87808229a09c10ee9b8bbc132
iii <iii@userteki-macbook.local>
2017-06-30 16:32:15 +0800

    v1

--- MyApp16/ViewController.swift
+++ MyApp16/ViewController.swift
@@ -9,15 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let fmgr = FileManager.default
+    let home = NSHomeDirectory()
+    var appdata:String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        print(home)
+        
+        appdata = home + "/Documents/mydaya"
+        
+        if !fmgr.fileExists(atPath: appdata!){
+            do {
+                try fmgr.createDirectory(atPath: appdata!, withIntermediateDirectories: true, attributes: nil)
+                print("create Dir")
+            }catch{
+                
+            }
+        }else {
+            print("Dir OK")
+        }
+        
+        
+//        do {
+////            try fmgr.createDirectory(atPath: newdir, withIntermediateDirectories: true, attributes: nil)
+//            
+////            try fmgr.copyItem(atPath: newdir, toPath: copydir)
+//            
+//            try fmgr.removeItem(atPath: copydir)
+//            
+//            
+//            print("OK")
+//        }catch {
+//            print(error)
+//        }
     }
 
 
